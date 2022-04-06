2992b16b93005e766fdc00e438e79022e8006956
hnjxy <103125657+hnjxy@users.noreply.github.com>
2022-04-06 22:15:55 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "936aa09f-1548-4a21-b8d4-2b65c7365ebf",
                         "alterId": 0
                     }
                 ],
